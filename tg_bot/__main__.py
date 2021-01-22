4ba9264da3923f7f37ad60677e86b08a00f8b715
ALEN TL <alen@pop-os.localdomain>
2021-01-22 22:55:37 +0530

    Updated Bot to V3

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -150,9 +150,9 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 Make Own Admin Bot", url="https://youtu.be/W6CLKrehy6w")],
+                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="🤖 Make Own Admin Bot", url="https://youtu.be/9RVLWgRTkYk")],
                      [InlineKeyboardButton(text="👥 Filmy Group", url="https://t.me/focusmoviess"), InlineKeyboardButton(text="🔔 Movie Channel", url="https://t.me/filmcompany_alen")],
-                     [InlineKeyboardButton(text="🎬 Youtube Channel", url="https://www.youtube.com/stealthtechnogaming?sub_confirmation=1"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
+                     [InlineKeyboardButton(text="🎬 Youtube Channel", url="https://www.youtube.com/channel/UCoWo4AIhxjEX-xCk0LwAtDA"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
